d617f50a0e34ea9faeb4e05e43dd7cdc1132f988
Weerawi <weerawikramatharindu@gmail.com>
2024-08-14 14:56:30 +0530

    aos install

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import NicForm from "./Pages/NicForm";
 import Summary from "./Pages/Summary";
 import Home from "./Pages/Home";
 import LoginPage from "./Pages/LoginPage";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { NicContext } from "./Context/NicContext";
-
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import AOS from "aos";
+import "aos/dist/aos.css";
  
 
 function App() {
+
+  //AOS initializing
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  });
+
 
   const { isSignedIn } = useContext(NicContext);
 
@@ -19,6 +29,8 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<LoginPage/>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/signin" element={<SignIn/>} />
           <Route path="/home" element={isSignedIn ?<Home /> : <LoginPage/>} />
           <Route path="/nicform" element={isSignedIn ?<NicForm /> : <LoginPage/>}  />
           <Route path="/summary" element={isSignedIn ?<Summary /> : <LoginPage/>} />
