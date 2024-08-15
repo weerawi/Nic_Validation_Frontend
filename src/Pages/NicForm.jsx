f277c1f43f3002b03ab5d571f7335b2250d835a2
Weerawi <weerawikramatharindu@gmail.com>
2024-08-16 01:23:21 +0530

    filename also added for nic validate list

--- src/Pages/NicForm.jsx
+++ src/Pages/NicForm.jsx
@@ -30,22 +30,24 @@ const NicForm = () => {
     return (
         <div className=" flex flex-col items-center w-[80%] mx-auto   mb-20 min-h-screen " data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="1000"> 
             <Title title="NIC Details"/>
-            <div className="font-semibold items-center bg-black text-white grid grid-cols-5 gap-1 md:gap-2 lg:gap-10 py-1 justify-around lg:w-[900px] md:w-[800px] w-[600px] ">
+            <div className="font-semibold items-center py-3 bg-black text-white grid grid-cols-6 gap-1 md:gap-2 lg:gap-10 justify-around lg:w-[900px] md:w-[800px] w-[600px] ">
                 <p className='flex justify-center'>Id</p>
                 <p className='flex justify-center'>NIC</p>
                 <p className='flex justify-center'>Birthday</p>
                 <p className='flex justify-center'>Age</p>
                 <p className='flex justify-center'>Gender</p>
+                <p className='flex justify-center'>FileName</p>
             </div>
 
             <div className="my-5 py-5 overflow-y-scroll h-auto bg-gray-100 p-5">
                 {nics.map((nic, i) => (
-                    <div key={i} className="lg:w-[900px] md:w-[800px] w-[600px] items-center border-b-2 grid grid-cols-5 gap-1 md:gap-2 lg:gap-10 py-2 border-gray-400">
+                    <div key={i} className="lg:w-[900px] md:w-[800px] w-[600px] items-center border-b-2 grid grid-cols-6 gap-1 md:gap-2 lg:gap-10 py-2 border-gray-400">
                         <p className='flex justify-center'>{i + 1 + (page * size)}</p>
                         <p className='flex justify-center'>{nic.nicNumber}</p>
                         <p className='flex justify-center'>{nic.dob}</p>
                         <p className='flex justify-center'>{nic.age}</p>
                         <p className='flex justify-center'>{nic.gender}</p>
+                        <p className='flex justify-center'>{nic.fileName}</p>
                     </div>
                 ))}
             </div>
